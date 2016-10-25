696dd46baeb6b94c586e08ee48d05a7459fd0f56
yekaterinashenets <yekaterina.shenets@gmail.com>
2016-10-25 20:41:14 +0300

    main.js was changed

--- tumblr/js/main.js
+++ tumblr/js/main.js
@@ -1,10 +1,11 @@
 "use strict";
 let token = 'O4EwAgqignUjMlpU0WfwE9elsI6jTymAmScBAAMK8PsHOHviiq',
-blog_name="russianquotes.tumblr.com";
+blog_name="russianquotes.tumblr.com",
+tag="жизнь";
 let getInfo = () => {
     $.ajax({ 
             type: 'GET',
-            url:'http://api.tumblr.com/v2/blog/'+blog_name+'/posts/quote?api_key='+token+'&tag=жизнь',
+            url:'http://api.tumblr.com/v2/blog/'+blog_name+'/posts/quote?api_key='+token+'&tag='+tag,
             dataType:'jsonp',
             headers: {
                     'api_key': token,
