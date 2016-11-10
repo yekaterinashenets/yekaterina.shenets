9a5a5dd0085f8b1d7f22a0c28a0bc1375bb1ce19
yekaterinashenets <yekaterina.shenets@gmail.com>
2016-11-10 17:38:27 +0300

    solution was changed

--- tumblr/js/main.js
+++ tumblr/js/main.js
@@ -1,27 +1,36 @@
 "use strict";
-let token = 'O4EwAgqignUjMlpU0WfwE9elsI6jTymAmScBAAMK8PsHOHviiq',
+let token = 'DaUnfPNXSAWwKQaPkCWCFbRB0E9S92yX0PwpBs9rgMHdwAkSeo',
 blog_name="russianquotes.tumblr.com",
-tag="жизнь";
+tages=["life", "ветви дуба", "молко", "рэй брэдбери"];
+var i=0;
 let getInfo = () => {
     $.ajax({ 
             type: 'GET',
-            url:'http://api.tumblr.com/v2/blog/'+blog_name+'/posts/quote?api_key='+token+'&tag='+tag,
+            url:'http://api.tumblr.com/v2/blog/'+blog_name+'/posts/quote?api_key='+token+'&tag='+tages[i],
             dataType:'jsonp',
             headers: {
                     'api_key': token,
             },
-             success:function(blog) { 
+             success: 
+             function(blog) { 
                 console.log(blog);
+                document.getElementById("header").innerHTML='Цитаты  блога russianquotes из tumblr, содержащие #'+tages[i];
                  let template = Handlebars.compile( $('#template').html()  );
                  $('.posts').empty().append( template(blog)  );
+                 i++;
+                if(i==tages.length)
+                    i=0;
              },
-             error:function() {
-                     console.log("error"); 
+             error:function(xhr, ajaxOptions, thrownError) {
+                    document.getElementById("error").style.display="block";
+                    document.getElementById("error").innerHTML=xhr.status + ': ' + xhr.statusText;
+                    $('#loading').hide();
              }
      });
+     
 }
 
 $(document).ready(function () {
     getInfo();
-    setInterval(getInfo, 10000);
+    setInterval(getInfo, 3000);
 })
